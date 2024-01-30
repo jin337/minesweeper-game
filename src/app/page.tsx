b8c44072be7abd934c77312ff47736993cca218a
jin <jin337x@outlook.com>
2024-01-30 12:21:05 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,16 @@ const Home = () => {
   ));
 
   return (
-    <main className="wrap-container mx-auto m-10 select-none">
-      <h1 className="text-lg text-red-500"> be under construction~</h1>
-      <div className="p-10 bg-gradient-to-r from-indigo-950 via-sky-950 to-emerald-950 text-neutral-100">
-        {/* <div className="text-base">
-          <div className="flex gap-2">
-            <div className=" bg-blue-500 px-2 py-1 rounded cursor-pointer" onClick={() => setBox(9)}>9*9</div>
-            <div className=" bg-blue-500 px-2 py-1 rounded cursor-pointer" onClick={() => setBox(16)}>16*16</div>
-          </div>
-        </div> */}
-        <div className="mt-10">
+    <main className="wrap-container mx-auto h-screen select-none bg-gradient-to-r from-indigo-950 via-sky-950 to-emerald-950 text-neutral-100">
+      {/* <div className="text-base">
+        <div className="flex gap-2">
+          <div className=" bg-blue-500 px-2 py-1 rounded cursor-pointer" onClick={() => setBox(9)}>9*9</div>
+          <div className=" bg-blue-500 px-2 py-1 rounded cursor-pointer" onClick={() => setBox(16)}>16*16</div>
+        </div>
+      </div> */}
+      <div className="p-10">
+        <h1 className="text-lg text-red-500 text-center">be under construction~</h1>
+        <div className="p-10">
           <div className="grid grid-cols-9 gap-1 w-[255px] xs:w-[400px] m-auto">
             {gridItems}
           </div>
